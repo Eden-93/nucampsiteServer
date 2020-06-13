a846fbfbdb8e63dfac027d2ea2eb4f1b1e01230d
Eden Ryan <eden.ryan@rocketmail.com>
2020-06-13 13:39:05 -0400

    Week 3 Assignment

--- authenticate.js
+++ authenticate.js
@@ -38,3 +38,13 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = function(req, res, next) {
+    if (req.user.admin) {
+        next();
+    } else {
+        const err = new Error('You are not authorized to perform the operation!');
+        err.status = 403;
+        return next(err);
+    }
+}
